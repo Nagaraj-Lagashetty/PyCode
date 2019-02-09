34a9bd7e579f8ed940ffaa56063cdcefcdaf6eb3
Nagaraj-Lagashetty <nagara.lagashetty77@gmail.com>
2019-02-10 01:52:01 +0530

    V1.0

--- RegexSearch.py
+++ RegexSearch.py
@@ -1,13 +1,19 @@
-rows = input('enter No. of rows')
-print(rows)
-n = 1
-while n <= (int(rows)):
-    print('*'*n)
-    n += 1
-
-print('--------------------')
-
-for i in range(1,int(rows)+1):
-    for j in range(1,i+1):
-        print('*',end='')
-    print()
+#Regex search thru multiple text files
+import re
+import os
+print(os.getcwd())
+os.chdir(input('Enter directory in which you search files for pattern\n'))
+#'c:\\users\\user\\desktop\\pylife\\filehandling'  = path
+print(os.getcwd())
+files = os.listdir()
+word = input('Enter the word you want search\n')
+for i in files :
+    f = open(os.path.join(os.getcwd(),i),'r+')
+    #g = re.search(input(),f.read())
+    #g = re.findall(r'\d\d\d\d\d\d\d\d\d\d',f.read())
+    #g = re.findall(word,f.read())
+    g = re.sub(word,'Hello',f.read())
+    #f.seek(0)
+    f.write(g)
+    print('{} - {}'.format(i,g))
+    f.close()
